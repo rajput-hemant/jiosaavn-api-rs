ea85058383c253b74a09b00d57a5b24b2d52ba8d
rajput-hemant <rajput.hemant2001@gmail.com>
2023-08-05 11:34:58 +0530

    init
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+<div align=center>
+
+# Jio Saavn API in Rust
+
+### A simple wrapper for the Jio Saavn API written in Rust.
+
+## Installation
+
+</div>
+
+```
+git clone https://github.com/rajput-hemant/jiosaavn-api-rs
+cd jiosaavn-api-rs
+cargo run
+```
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("🚀 Server listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, Folks!"
+}
