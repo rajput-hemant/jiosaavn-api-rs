3accd84a9ffaf7861c6c2824bbfcd3ca6ae00d2a
rajput-hemant <rajput.hemant2001@gmail.com>
2023-08-10 12:08:24 +0530

    feat: add rate limit layer from tower
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,15 @@ pub mod payloads;
 pub mod services;
 pub mod utils;
 
-use axum::{http::Method, routing::get, Router};
+use axum::{
+    error_handling::HandleErrorLayer,
+    http::{Method, StatusCode},
+    routing::get,
+    BoxError, Router,
+};
+use dotenv::dotenv;
+use std::{env, time::Duration};
+use tower::{buffer::BufferLayer, limit::RateLimitLayer, ServiceBuilder};
 use tower_http::cors::{Any, CorsLayer};
 
 use handlers::{
@@ -17,7 +25,14 @@ use handlers::{
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let router = Router::new()
+    dotenv().ok();
+
+    let enable_rate_limit = env::var("ENABLE_RATE_LIMIT")
+        .unwrap_or_else(|_| "false".to_string())
+        .parse()
+        .unwrap_or(false);
+
+    let mut router = Router::new()
         .route("/", get(root))
         // home modules / launch data
         .route("/modules", get(modules_handler))
@@ -43,6 +58,21 @@ async fn main() {
                 .allow_origin(Any)
                 .allow_methods([Method::GET]),
         );
+
+    // rate limit layer
+    if enable_rate_limit {
+        router = router.layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|err: BoxError| async move {
+                    (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Unhandled error: {}", err),
+                    )
+                }))
+                .layer(BufferLayer::new(1024))
+                .layer(RateLimitLayer::new(5, Duration::from_secs(1))),
+        );
+    }
 
     let addr = "[::]:8080".parse().unwrap();
 
