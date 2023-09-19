421b381ff22d467f92611269eeaac75f3ccecb7b
rajput-hemant <rajput.hemant2001@gmail.com>
2023-09-19 20:26:21 +0530

    fix: 404 route handler
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ api/[...404].rs
@@ -0,0 +1,17 @@
+use jiosaavn::handlers::not_found_handler;
+use serde_json::json;
+use vercel_runtime::{run, Body, Error, Request, Response};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    run(handler).await
+}
+
+pub async fn handler(_: Request) -> Result<Response<Body>, Error> {
+    let (status, payload) = not_found_handler().await;
+
+    Ok(Response::builder()
+        .status(status)
+        .header("Content-Type", "application/json")
+        .body(json!(payload.0).to_string().into())?)
+}
--- src/handlers/not_found_handler.rs
+++ src/handlers/not_found_handler.rs
@@ -1,13 +1,16 @@
-use axum::Json;
+use axum::{http::StatusCode, Json};
 
 use crate::config::DOCS_URL;
 use crate::models::response::{CResponse, Status};
 
 /// Handler for `404` route
-pub async fn not_found_handler() -> Json<CResponse<String>> {
-    Json(CResponse::new(
-        Status::Failed,
-        format!("Requested route not found, please check the documentation at {DOCS_URL}"),
-        None,
-    ))
+pub async fn not_found_handler() -> (StatusCode, Json<CResponse<String>>) {
+    (
+        StatusCode::NOT_FOUND,
+        Json(CResponse::new(
+            Status::Failed,
+            format!("Requested route not found, please check the documentation at {DOCS_URL}"),
+            None,
+        )),
+    )
 }
